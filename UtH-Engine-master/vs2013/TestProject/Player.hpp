4983679e88b6001f6a83fd081400600e502e0e37
Freeman117 <samu.tolonen@hotmail.com>
2014-10-10 11:27:48 +0300

    Added a small start for a player class.
    Engine update just came in.

---
+++ UtH-Engine-master/vs2013/TestProject/Player.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+#include <UtH/Engine/GameObject.hpp>
+#include <UtH/Engine/Engine.hpp>
+
+class Player : public uth::GameObject
+{
+public:
+	Player();
+	~Player();
+private:
+	void update(float dt) override;
+	void draw(uth::RenderTarget& target) override;
+
+};
+
+#endif
